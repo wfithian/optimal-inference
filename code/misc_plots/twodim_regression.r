36cc59f8468d687990a056b4cfec60231a9a4b09
Will Fithian <wfithian@gmail.com>
2017-04-14 22:46:45 -0700

    up to date for arxiv V3, fixed bug in Fig. 5b code

--- code/misc_plots/twodim_regression.r
+++ code/misc_plots/twodim_regression.r
@@ -16,6 +16,9 @@ lines(c(-y[2],-10),c(y[2],y[2]),lwd=2,col="brown")
 points(y,pch=16)
 dev.off()
 
+unnorm.cond.dens <- function(x) (1-2*pnorm(-abs(x)))*dnorm(x) #p(|Y2| < |Y1| = x)
+cond.dens <- function(x) unnorm.cond.dens(x) / 2 / integrate(function(u) unnorm.cond.dens(u),0,10)$value
+
 # Fig 5b
 pdf("fullvredNulls.pdf",height=4.3,width=4)
 par(mar=c(4.1,4.1,3.1,0.1),yaxs="i")
@@ -24,7 +27,7 @@ plot(x,(abs(x)>2.5)*dnorm(x)/2/pnorm(-2.5),ylim=c(0,1.4),lty=1,
       col="brown",type="l",
       main="Conditional Null Distributions",
       ylab="Density",xlab=expression(Y[1]))
-polygon(c(x,0),c(abs(x)*dnorm(x)/2/integrate(function(u) u*dnorm(u),0,10)$value,0),lty=2,col="#F4E918")
+polygon(c(x,0),c(cond.dens(x),0),lty=2,col="#F4E918")
 lines(x,(abs(x)>2.5)*dnorm(x)/2/pnorm(-2.5),col="brown")
 legend("topleft",legend=c("Saturated Model","Selected Model","Observed Value"),lty=1:3,bg="white", col=c("brown","black","black"))
 #norm.y <- sqrt(sum(y^2))
@@ -33,7 +36,7 @@ abline(v=2.9,lty=3)
 dev.off()
 
 # p-values for selected and saturated models
-integrate(function(x) abs(x)*dnorm(x)/integrate(function(u) u*dnorm(u), 0, 10)$value, 2.9,10)
+2*integrate(function(x) cond.dens(x), 2.9,10)$value
 pnorm(-2.9)/pnorm(-2.5)
 
 B <- 10000
@@ -43,7 +46,7 @@ for(b in 1:B) {
     y <- mu + rnorm(2)
     if(abs(y[1]) > abs(y[2])) {
         pvals <- rbind(pvals, c(
-            integrate(function(x) abs(x)*dnorm(x)/integrate(function(u) u*dnorm(u), 0, 10)$value, abs(y[1]),10)$value,
+            2*integrate(function(x) cond.dens(x), abs(y[1]),10)$value,
             pnorm(-abs(y[1]))/pnorm(-abs(y[2]))
             ))
     }
